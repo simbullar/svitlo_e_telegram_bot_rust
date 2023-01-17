71c88ee8caaaf8a44086b2e76297044c2eb332bd
Roman <86300574+simbullar@users.noreply.github.com>
2023-01-17 21:47:07 +0200

    Add files via upload

---
+++ src/token_gitignore.rs
@@ -0,0 +1,11 @@
+pub fn change(val: &mut &str) {
+    *val = "5467840593:AAEjO8TgiippPjZyjIr1f82LfDaAdsb0jD8";
+}
+
+pub fn change2(val: &mut &str) {
+    *val = "856546075";
+}
+
+pub fn change3(val: &mut &str) {
+    *val = "162.55.27.218";
+}
